c573c679ec58d1b96739c365095aa650f59fa3da
Saif Ali <saifali7646@gmail.com>
2021-07-29 23:28:12 +0000

    esLint the code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,25 +1,20 @@
 const request = require('request');
-const user_CLI_Input = process.argv;
+const userInput = process.argv;
 
-
-const breedDescriptionSearcher = function (){
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${user_CLI_Input[2]}`, (error, response, body) => {
-     // Print the error if one occurred
-    if(error){
-      console.log('error:', error); 
+const breedDescriptionSearcher = function() {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${userInput[2]}`, (error, response, body) => {
+    // Print the error if one occurred
+    if (error) {
+      console.log('error:', error);
     }
-    //If no error occured then proceed with the next steps   
-    else{
+    //If no error occured then proceed with the next steps
+    else {
 
-      breedDescription = JSON.parse(body); //Convert the string into an object,
-      console.log(breedDescription['0']['description']); // Print the HTML for the Google homepage.  
-    }    
+      const breedDescription = JSON.parse(body); //Convert the string into an object,
+      console.log(breedDescription['0']['description']); // Print the HTML for the Google homepage.
+    }
   });
 
-}
-
+};
 
 breedDescriptionSearcher();
-
-
-
